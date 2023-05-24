a629824ed4e6650a3d3b5bd4ffcfc24ca2b8f29e
chucoyos <unodev@outlook.com>
2023-05-23 19:26:20 -0600

    Style input group

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const forecast = async (location = 'Manzanillo, Col.') => {
 const renderforecastData = (data) => {
   console.log(data.current.is_day)
   const body = document.querySelector('body')
-  const main = document.querySelector('.main')
   if(data.current.is_day == 1) {
     body.style.backgroundImage = 'url("./images/sunySky.jpg")'
   } else {
@@ -22,7 +21,7 @@ const renderforecastData = (data) => {
   const header = document.querySelector('.header')
 
   const formGroup = document.createElement('div')
-  formGroup.classList.add('.formGroup')
+  formGroup.classList.add('formGroup')
   const locationInput = document.createElement('input')
   locationInput.classList.add('locationInput')
   locationInput.placeholder = 'Portland, Oregon'
@@ -39,25 +38,25 @@ const renderforecastData = (data) => {
   formGroup.appendChild(locationInput)
   formGroup.appendChild(confirmBtn)
   
-  const locationContainder = document.createElement('p')
-  locationContainder.classList.add('.locationContainer')
-  locationContainder.textContent = `🔎️ ${data.location.name} 📌️`
-  locationContainder.addEventListener('click', () => {
-    locationContainder.replaceWith(formGroup)
+  const locationContainer = document.createElement('p')
+  locationContainer.classList.add('locationContainer')
+  locationContainer.textContent = ` ${data.location.name} 🔎️`
+  locationContainer.addEventListener('click', () => {
+    locationContainer.replaceWith(formGroup)
   })
  
   const tempContainer = document.createElement('p')
-  tempContainer.classList.add('.tempContainer')
+  tempContainer.classList.add('tempContainer')
   tempContainer.textContent = data.current.temp_c + " \260C  /  " + data.current.temp_f + " \260F" 
 
   const conditionContainer = document.createElement('p')
-  conditionContainer.classList.add('.conditionContainer')
+  conditionContainer.classList.add('conditionContainer')
   conditionContainer.textContent = data.current.condition.text
 
   const conditionImage = document.createElement('img')
   conditionImage.src = data.current.condition.icon
 
-  header.appendChild(locationContainder)
+  header.appendChild(locationContainer)
   header.appendChild(tempContainer)
   header.appendChild(conditionImage)
   header.appendChild(conditionContainer)
@@ -65,16 +64,16 @@ const renderforecastData = (data) => {
   const confortCard = document.querySelector('.confortCard')
 
   const confortHeader = document.createElement('header')
-  confortHeader.classList.add('.confortHeader')
+  confortHeader.classList.add('confortHeader')
 
   const confortTitle = document.createElement('p')
-  confortTitle.classList.add('.confortTitle')
+  confortTitle.classList.add('confortTitle')
   confortTitle.textContent = 'CONFORT LEVEL'
   const humidityText = document.createElement('p')
-  humidityText.classList.add('.humidityText')
+  humidityText.classList.add('humidityText')
   humidityText.textContent = `Humidity: ${data.current.humidity} %`
   const humidityIcon = document.createElement('p')
-  humidityIcon.classList.add('.humidityIcon')
+  humidityIcon.classList.add('humidityIcon')
   humidityIcon.textContent = '💦️'
 
   confortHeader.appendChild(confortTitle)
@@ -84,16 +83,16 @@ const renderforecastData = (data) => {
 
   const windCard = document.querySelector('.windCard')
   const windTitle = document.createElement('p')
-  windTitle.classList.add('.windTitle')
+  windTitle.classList.add('windTitle')
   windTitle.textContent = 'WIND'
   const windIcon = document.createElement('p')
-  windIcon.classList.add('.windIcon')
+  windIcon.classList.add('windIcon')
   windIcon.textContent = '🌬️'
   const windSpeedElement = document.createElement('p')
-  windSpeedElement.classList.add('.windSpeedElement')
+  windSpeedElement.classList.add('windSpeedElement')
   windSpeedElement.textContent = ` Speed: ${data.current.wind_kph} kph / ${data.current.wind_mph} mph`
   const windDirectionElement = document.createElement('p')
-  windDirectionElement.classList.add('.windDirectionElement')
+  windDirectionElement.classList.add('windDirectionElement')
   windDirectionElement.textContent = `Direction: ${data.current.wind_dir}`
   
   windCard.appendChild(windTitle)
